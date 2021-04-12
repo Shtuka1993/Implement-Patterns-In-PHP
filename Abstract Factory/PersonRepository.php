4d397c3806d541a0f5413252dbf66162f03b19c8
yshtokaylo <yshtokaylo@artelogic.net>
2021-04-12 18:45:26 +0300

    Initialization of project

---
+++ Abstract Factory/PersonRepository.php
@@ -0,0 +1,6 @@
+<?php
+    interface PersonRepository {
+        public function savePerson(Person $person):void;
+        public function readPeople():array;
+        public function readPerson(string $name):?Person;
+    }
